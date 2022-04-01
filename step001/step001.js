979975915473e7eca3d7711622cdd74267ba09f6
Edmilson Santos de Almeida <99562810+EddAmbrosio@users.noreply.github.com>
2022-04-01 20:17:40 -0300

    Create step001.js

---
+++ step001/step001.js
@@ -0,0 +1,19 @@
+/*let book = {
+    topic: "JavaScript",
+    edition: 7
+}
+
+console.log(book["edition"]);
+console.log(book.topic);
+*/
+
+/*let count = 0
+count ++
+console.log(count)
+count --
+console.log(count)
+count += 2
+console.log(count)*/
+
+let x = 2; let y = 3;
+!(x === 3)
